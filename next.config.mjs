4f81d11553f440e21c6d411efb1ba62f3bdea915
Abel <abel@example.com>
2026-05-11 17:22:44 -0400

    Update next.config.mjs: add standalone output, ignore build errors, and unoptimize images

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true
+  },
   experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
     serverActions: {
       bodySizeLimit: '2mb',
     },
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
